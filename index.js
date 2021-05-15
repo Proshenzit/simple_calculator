99d3c97a092d39609a83cc086925e705e1802101
Proshenzit <37993972+Proshenzit@users.noreply.github.com>
2021-05-15 05:03:58 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+
+// var  base=prompt("Enter the Number");
+
+// var  base=prompt("Enter the Number");
+
+ 
+
+
+
+// var text= "bangladesh";
+// text.length;
+
+var base=prompt("Enter Base =");
+
+var height=prompt("Enter Height =");
+
+var asrea
